4430b5521ee1d8a593c1dac0921efa8fc9f5d0c3
MuqtadirBillah <abbasimuqtadir@gmail.com>
2022-10-04 18:37:47 +0500

    nextjs starter file

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+            <meta charSet="utf-8" />
+            <title>Next.js website</title>
+            <meta name="keywords" content="medium, blog, article, next.js" />
+            <meta name="description" content="Test website for nextjs seo" />
+      </Head>
+      <h1>Home Page</h1>
+    </div>
+  )
+}
---
+++ pages/user/view/[id].jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { useRouter } from 'next/router'
+import axios from 'axios';
+
+function ViewUser(){
+
+    var [user, setUser] = useState();
+    const router = useRouter()
+
+    useEffect(()=>{
+        const { id } = router.query
+        axios.get(`http://localhost:3000/api/user/view/${id}`)
+        .then((response) =>{
+            console.log(response)
+            setUser(response.data.data)
+        })
+        .catch(err=>console.log(err));
+    }, [])
+
+    return(
+        <div className="viewUser">
+            <h1>User Details</h1>
+            <h5>Id: {user.id}</h5>
+            <h5>Name: {user.name}</h5>
+        </div>
+    )
+}
+export default ViewUser;
